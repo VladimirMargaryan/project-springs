219b32e80bcfc52e591be5bde6e22d31e001e98d
Vladimir Margaryan <vlad.margaryan98@gmail.com>
2023-04-15 21:45:50 +0400

    Implemented Spring and SpringArray classes

--- src/model/Spring.java
+++ src/model/Spring.java
@@ -1,51 +1,62 @@
 package model;
 
+import java.util.Arrays;
+
 public class Spring {
 
-    private double k;
+    private double k = 1.0;
 
-    public Spring() {
-        this.k = 1d;
-    }
+    public Spring() {}
 
     public Spring(double k) {
         this.k = k;
     }
 
-    public double getK() {
+    public double getStiffness() {
         return k;
     }
 
-    private void setK(double k) {
+    private void setStiffness(double k) {
         this.k = k;
     }
 
-    public Spring inSeries(Spring that) {
-        //TODO: Implement method
-        return null;
-    }
-
-    public Spring inParallel(Spring that) {
-        //TODO: Implement method
-        return null;
-    }
-
     public double[] move(double t, double dt, double x0, double v0) {
-        //TODO: Implement method
-        return null;
+        final int n = (int) (t / dt) + 1;
+        final double[] x = new double[n];
+        final double omega = Math.sqrt(k);
+        final double B = v0 / omega;
+        Arrays.setAll(x, i -> x0 * Math.cos(omega * i * dt) + B * Math.sin(omega * i * dt));
+
+        return x;
     }
 
     public double[] move(double t, double dt, double x0) {
-        return move(t, dt, x0, 0);
+        return move(t, dt, x0, 0.0);
     }
 
     public double[] move(double t0, double t1, double dt, double x0, double v0) {
-        //TODO: Implement method
-        return null;
+        return move(t0, t1, dt, x0, v0, 1.0);
     }
 
     public double[] move(double t0, double t1, double dt, double x0, double v0, double m) {
-        //TODO: Implement method
-        return null;
+        final int n = (int) ((t1 - t0) / dt) + 1;
+        final double[] x = new double[n];
+        final double omega = Math.sqrt(k / m);
+        final double B = v0 / omega - x0 * omega;
+        Arrays.setAll(x, i -> x0 * Math.cos(omega * (i * dt - t0)) + B * Math.sin(omega * (i * dt - t0)));
+
+        return x;
+    }
+
+    public Spring inSeries(Spring that) {
+        final double newK = this.k + that.k;
+
+        return new Spring(newK);
+    }
+
+    public Spring inParallel(Spring that) {
+        final double newK = 1.0 / (1.0 / this.k + 1.0 / that.k);
+
+        return new Spring(newK);
     }
 }
--- src/model/SpringArray.java
+++ src/model/SpringArray.java
@@ -1,14 +1,77 @@
 package model;
 
+import java.util.*;
+
 public class SpringArray {
 
     public static Spring equivalentSpring(String springExpr) {
-        //TODO: Implement method
-        return null;
+        final Stack<Spring> stack = new Stack<>();
+
+        for (char c : springExpr.toCharArray()) {
+            if (c == '{') {
+                stack.push(new Spring());
+            } else if (c == '[') {
+                stack.push(null);
+            } else if (c == '}') {
+                Spring s = stack.pop();
+                while (stack.peek() != null) {
+                    s = stack.pop().inSeries(s);
+                }
+                stack.pop();
+                stack.push(s);
+            } else if (c == ']') {
+                Spring s = stack.pop();
+                while (stack.peek() != null) {
+                    s = stack.pop().inParallel(s);
+                }
+                stack.pop();
+                stack.push(s);
+            }
+        }
+
+        return stack.pop();
     }
 
     public static Spring equivalentSpring(String springExpr, Spring[] springs) {
-        //TODO: Implement method
-        return null;
+        final HashMap<Character, Spring> map = new HashMap<>();
+        final char[] chars = springExpr.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '{' || chars[i] == '[') {
+                continue;
+            } else if (chars[i] == '}' || chars[i] == ']') {
+                continue;
+            } else {
+                map.put(chars[i], springs[i]);
+            }
+        }
+
+        Stack<Spring> stack = new Stack<>();
+
+        for (char c : chars) {
+            if (c == '{') {
+                stack.push(new Spring());
+            } else if (c == '[') {
+                stack.push(null);
+            } else if (c == '}') {
+                Spring s = stack.pop();
+                while (stack.peek() != null) {
+                    s = stack.pop().inSeries(s);
+                }
+                stack.pop();
+                stack.push(s);
+            } else if (c == ']') {
+                Spring s = stack.pop();
+                while (stack.peek() != null) {
+                    s = stack.pop().inParallel(s);
+                }
+                stack.pop();
+                stack.push(s);
+            } else {
+                stack.push(map.get(c));
+            }
+        }
+
+        return stack.pop();
     }
 }
